3824e2ff1313d26af4fd5d2c56d67a224c5c88f9
Basmala-Abdullah <basmala.abdullah.ahmed@gmail.com>
2025-03-09 01:56:40 +0200

    feat: Implement order history page

---
+++ bookstore-frontend/src/app/order-history/order-history.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../services/order/order.service'
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-order-history',
+  imports: [],
+  templateUrl: './order-history.component.html',
+  styleUrl: './order-history.component.css'
+})
+export class OrderHistoryComponent implements OnInit {
+  orders: any[] =[];
+
+  constructor(
+    private orderService: OrderService,
+    private route: ActivatedRoute
+  ) {} 
+
+  
+  ngOnInit(): void {
+    const userId = this.route.snapshot.paramMap.get('id');
+    
+    if (userId) {
+      this.orderService.getUserOrders(userId)?.subscribe(data => {
+        if(data){
+          this.orders = data;
+          console.log(this.orders);
+        }
+      });
+    }
+  }
+}
