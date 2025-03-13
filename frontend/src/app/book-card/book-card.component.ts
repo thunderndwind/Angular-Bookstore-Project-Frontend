f2147a700cbdca7f62af3e99684345a38340ab5a
Esraa <esraakhalifa5@gmail.com>
2025-03-13 06:56:51 +0200

    WIP: Add book card component with styling

--- frontend/src/app/book-card/book-card.component.ts
+++ frontend/src/app/book-card/book-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-book-card',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './book-card.component.css'
 })
 export class BookCardComponent {
-
+  @Input() book: any;
 }
